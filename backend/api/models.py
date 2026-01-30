70c95659107e5b496dd7956fcbfc6a93a83e3511
n1b11 <nburd@clarku.edu>
2026-01-30 11:38:26 -0500

    Create models

---
+++ backend/api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Expense(models.Model):
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    recipients = models.ManyToManyField('User', related_name='expenses')
+
+class Payment(models.Model):
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    recipient = models.ForeignKey('User', on_delete=models.CASCADE, related_name='payments')
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+
+    
